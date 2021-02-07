f69af88cff292ec3393671109307572d4a513aca
Caleb Georgeson <geo16004@byui.edu>
2021-02-07 16:04:37 -0700

    added scripture books

--- __init__.py
+++ __init__.py
@@ -1,5 +1,95 @@
-from mycroft import MycroftSkill, intent_file_handler
+from mycroft import MycroftSkill, intent_file_handler, utils
 import random
+
+books = [
+ "Genesis",
+ "Exodus",
+ "Leviticus",
+ "Numbers",
+ "Deuteronomy",
+ "Joshua",
+ "Judges",
+ "Ruth",
+ "1 Samuel",
+ "2 Samuel",
+ "1 Kings",
+ "2 Kings",
+ "1 Chronicles",
+ "2 Chronicles",
+ "Ezra",
+ "Nehemiah",
+ "Esther",
+ "Job",
+ "Psalms",
+ "Proverbs",
+ "Ecclesiastes",
+ "Song of Solomon",
+ "Isaiah",
+ "Jeremiah",
+ "Lamentations",
+ "Ezekiel",
+ "Daniel",
+ "Hosea",
+ "Joel",
+ "Amos",
+ "Obadiah",
+ "Jonah",
+ "Micah",
+ "Nahum",
+ "Habakkuk",
+ "Zephaniah",
+ "Haggai",
+ "Zechariah",
+ "Malachi",
+ "Matthew",
+ "Mark",
+ "Luke",
+ "John",
+ "Acts",
+ "Romans",
+ "1 Corinthians",
+ "2 Corinthians",
+ "Galatians",
+ "Ephesians",
+ "Philippians",
+ "Colossians",
+ "1 Thessalonians",
+ "2 Thessalonians",
+ "1 Timothy",
+ "2 Timothy",
+ "Titus",
+ "Philemon",
+ "Hebrews",
+ "James",
+ "1 Peter",
+ "2 Peter",
+ "1 John",
+ "2 John",
+ "3 John",
+ "Jude",
+ "Revelation",
+ "1 Nephi",
+ "2 Nephi",
+ "Jacob",
+ "Enos",
+ "Jarom",
+ "Omni",
+ "Words of Mormon",
+ "Mosiah",
+ "Alma",
+ "Helaman",
+ "3 Nephi",
+ "4 Nephi",
+ "Mormon",
+ "Ether",
+ "Moroni",
+ "Doctrine and Covenants",
+ "Moses",
+ "Abraham",
+ "Joseph Smith--Matthew",
+ "Joseph Smith--History",
+ "Articles of Faith"
+]
 
 class ScriptureReader(MycroftSkill):
     def __init__(self):
@@ -7,8 +97,9 @@ class ScriptureReader(MycroftSkill):
 
     @intent_file_handler('reader.scripture.intent')
     def handle_reader_scripture(self, message):
-
-        self.speak_dialog('reader.scripture')
+       numbers = utils.parse.extract_numbers(message)
+       print(numbers)
+        self.speak("The numbers are: {}".format(numbers))
 
 
 def create_skill():
